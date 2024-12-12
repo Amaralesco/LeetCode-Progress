13a83a64ca47d0ac48bbc4e671c6e316cb9f9613
Amaralesco <65781292+Amaralesco@users.noreply.github.com>
2024-12-12 11:37:29 +0000

    12/12

---
+++ heap/047 #2558. Take Gifts From the Richest Pile.cpp
@@ -0,0 +1,21 @@
+/*   
+    Neetcode: Solved 30/150
+    Leetcode: Solved 23 Easy 20 Medium
+
+ 
+Learnt a little about heap
+
+*/
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+
+        map<int,int> tmp_map[k];
+        for (int i = 0; i<k; i++){
+        tmp_map[i] = gifts[i];
+        }
+        for (int i = 0; i<gifts.size(); i++){
+            
+        }
+    }
+};
